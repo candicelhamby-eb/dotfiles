74e05e3158829d3abdae2b20f28fd7344d5d6a91
candicelhamby-eb <chamby@eventbrite.com>
2018-02-23 15:01:37 -0600

    updating files

--- working/install.bash
+++ working/install.bash
@@ -7,18 +7,18 @@ update_installed_lists() {
 
 
 install_brew_stuff() {
-  for package in $(cat installed_via_brew.txt);
+  for package in $(cat ~/dotfiles/working/installed_via_homebrew.txt);
     do
       brew install $package
   done
 
-  for package in $(cat installed_via_cask.txt)
+  for package in $(cat ~/dotfiles/working/installed_via_cask.txt)
     do
-      brew install $package
+      brew cask install $package
   done
 
   # install misc brew stuff
-  for package in $(cat brew_apps.txt)
+  for package in $(cat ~/dotfiles/working/brew_apps.txt)
     do
       brew install $package
   done
@@ -34,7 +34,7 @@ install_eventbrite_repos(){
 
   # git close all of the eventbrite repos that you currently
   # have downloaded
-  for repo in $(ls eventbrite_repo_names.txt)
+  for repo in $(cat ~/dotfiles/working/eventbrite_repo_names.txt)
     do
       git clone "$EB_GITHUB_URL$repo.git"
   done
@@ -46,3 +46,11 @@ install_eventbrite_repos(){
 install_presto_stuff() {
   echo “follow instructions at https://docs.evbhome.com/data-foundry/tools_presto.html#access-via-presto-cli”
 }
+
+main() {
+    install_brew_stuff
+    install_eventbrite_repos
+    install_presto_stuff
+}
+
+main
